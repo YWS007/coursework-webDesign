718277379e77f3c925684c1259e53051c52eb935
Yw S <soonyw95@hotmail.com>
2023-11-17 22:45:30 +0800

    files added

---
+++ _vti_cnf/Slideshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-A4SDTVR\\soony
+vti_modifiedby:SR|DESKTOP-A4SDTVR\\soony
+vti_timelastmodified:TR|16 Nov 2023 15:41:17 -0000
+vti_timecreated:TR|22 Nov 2022 13:36:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|onlineevents.html
+vti_nexttolasttimemodified:TW|22 Nov 2022 13:36:35 -0000
+vti_cacheddtm:TX|16 Nov 2023 15:41:17 -0000
+vti_filesize:IR|743
---
+++ _vti_cnf/ddaccordion.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-A4SDTVR\\soony
+vti_modifiedby:SR|DESKTOP-A4SDTVR\\soony
+vti_timelastmodified:TR|16 Nov 2023 15:41:15 -0000
+vti_timecreated:TR|20 Nov 2022 13:16:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|committee.html
+vti_nexttolasttimemodified:TW|20 Nov 2022 13:19:02 -0000
+vti_cacheddtm:TX|16 Nov 2023 15:41:15 -0000
+vti_filesize:IR|15958
